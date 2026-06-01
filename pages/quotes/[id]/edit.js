753296e3efd868c9e9a50f77f3ff7c6e52febb17
lcs021099-create <lcs021099-create@users.noreply.github.com>
2026-06-01 21:27:55 +0800

    Fix import paths in quotes/[id]/edit.js (production build failure)

--- pages/quotes/[id]/edit.js
+++ pages/quotes/[id]/edit.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import QuoteForm from './new';
-import { quotesAPI } from '../../lib/api';
-import Layout from '../../components/Layout';
+import QuoteForm from '../new';
+import { quotesAPI } from '../../../lib/api';
+import Layout from '../../../components/Layout';
 
 export default function EditQuote() {
   const router = useRouter();
